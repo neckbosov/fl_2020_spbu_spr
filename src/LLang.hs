f0776e6688a26821d42dff8f49fbec4e71c59570
Nikita Bosov <neckbosov@gmail.com>
2020-04-08 22:18:16 +0300

    done eval

--- src/LLang.hs
+++ src/LLang.hs
@@ -64,8 +64,96 @@ parseWrite = strEq "write" *> fmap Write parseBracketsExpr <* symbol ';'
 parseInstruction :: Parser String String LAst
 parseInstruction = parseAssign <|> parseIf <|> parseWhile <|> parseRead <|> parseWrite
 
+fromBool :: Bool -> Int
+fromBool True  = 1
+fromBool False = 0
+
+toBool :: Int -> Bool
+toBool n | n /= 0 = True
+         | otherwise = False
+
+computeLExpr :: AST -> Subst -> Maybe Int
+computeLExpr (Num x)   _         = Just x
+computeLExpr (Ident x) s = Map.lookup x s
+computeLExpr (BinOp Plus x y) s  = do
+    lhs <- computeLExpr x s
+    rhs <- computeLExpr y s
+    return $ lhs + rhs
+computeLExpr (BinOp Mult x y) s  = do
+    lhs <- computeLExpr x s
+    rhs <- computeLExpr y s
+    return $ lhs * rhs
+computeLExpr (BinOp Minus x y) s = do
+    lhs <- computeLExpr x s
+    rhs <- computeLExpr y s
+    return $ lhs - rhs
+computeLExpr (BinOp Div x y) s   = do
+    lhs <- computeLExpr x s
+    rhs <- computeLExpr y s
+    True <- return (rhs /= 0)
+    return $ lhs `div` rhs
+computeLExpr (BinOp Pow x y) s   = do
+    lhs <- computeLExpr x s
+    rhs <- computeLExpr y s
+    True <- return (rhs >= 0)
+    return $ lhs ^ rhs
+computeLExpr (BinOp Equal x y) s = do
+    lhs <- computeLExpr x s
+    rhs <- computeLExpr y s
+    return $ fromBool (lhs == rhs)
+computeLExpr (BinOp Nequal x y) s = do
+    lhs <- computeLExpr x s
+    rhs <- computeLExpr y s
+    return $ fromBool (lhs /= rhs)
+computeLExpr (BinOp Gt x y) s = do
+    lhs <- computeLExpr x s
+    rhs <- computeLExpr y s
+    return $ fromBool (lhs > rhs)
+computeLExpr (BinOp Ge x y) s = do
+    lhs <- computeLExpr x s
+    rhs <- computeLExpr y s
+    return $ fromBool (lhs >= rhs)
+computeLExpr (BinOp Lt x y) s = do
+    lhs <- computeLExpr x s
+    rhs <- computeLExpr y s
+    return $ fromBool (lhs < rhs)
+computeLExpr (BinOp Le x y) s = do
+    lhs <- computeLExpr x s
+    rhs <- computeLExpr y s
+    return $ fromBool (lhs <= rhs)
+computeLExpr (BinOp And x y) s = do
+    lhs <- computeLExpr x s
+    rhs <- computeLExpr y s
+    return $ fromBool (toBool lhs && toBool rhs)
+computeLExpr (BinOp Or x y) s = do
+    lhs <- computeLExpr x s
+    rhs <- computeLExpr y s
+    return $ fromBool (toBool lhs || toBool rhs)
+computeLExpr (UnaryOp Minus x) s = (\y -> (-y)) <$> computeLExpr x s
+computeLExpr (UnaryOp Not x) s = (\y -> (fromBool . not . toBool) y) <$> computeLExpr x s
+
 initialConf :: [Int] -> Configuration
 initialConf input = Conf Map.empty input []
 
 eval :: LAst -> Configuration -> Maybe Configuration
-eval = error "eval not defined"
+eval (Seq ls) conf = foldl (\c ast -> (c >>= eval ast)) (Just conf) ls
+eval (If cond thn els) conf = do
+    c <- computeLExpr cond (subst conf)
+    res <- if toBool c then eval thn conf else eval els conf
+    return res
+eval ast@(While cond body) conf = do
+    c <- computeLExpr cond (subst conf)
+    let nconf = if toBool c then eval body conf else Nothing
+    res <- (nconf >>= eval ast) <|> Just conf
+    return res
+eval (Assign v e) conf = do
+    val <- computeLExpr e (subst conf)
+    let nsubst = Map.alter ((const . Just) val) v (subst conf)
+    return $ Conf nsubst (input conf) (output conf)
+eval (Write e) conf = do
+    val <- computeLExpr e (subst conf)
+    return $ Conf (subst conf) (input conf) (val : output conf)
+eval (Read v) conf = do
+    False <- return $ null (input conf)
+    let nsubst = Map.alter ((const . Just . head . input) conf) v (subst conf)
+    return $ Conf nsubst ((tail . input) conf) (output conf) 
