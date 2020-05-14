1e63ab25859138d9cc26572a7692b6a3be43c2ae
Nikita Bosov <neckbosov@gmail.com>
2020-05-14 16:53:28 +0300

    done hw11

--- src/Regexp.hs
+++ src/Regexp.hs
@@ -1,6 +1,6 @@
 module Regexp where
 
-import Prelude hiding (seq)
+import           Prelude hiding (seq)
 
 data Regexp = Empty
             | Epsilon
@@ -14,7 +14,20 @@ match :: Regexp -> String -> Bool
 match r s = nullable (foldl (flip derivative) r s)
 
 derivative :: Char -> Regexp -> Regexp
-derivative = error "derivative not implemented"
+derivative _ Empty    = Empty
+derivative _ Epsilon  = Empty
+derivative c (Char a) = if c == a then Epsilon else Empty
+derivative c (Seq r s) = Alt r1 r2
+    where
+        r1 = Seq (derivative c r) s
+        r2 = if nullable r then derivative c s else Empty
+derivative c (Alt r s) = Alt (derivative c r) (derivative c s)
+derivative c (Star r) = Seq (derivative c r) (Star r)
 
 nullable :: Regexp -> Bool
-nullable = error "nullable not implemented"
+nullable Empty     = False
+nullable Epsilon   = True
+nullable (Char _)  = False
+nullable (Alt r s) = nullable r || nullable s
+nullable (Seq r s) = nullable r && nullable s
+nullable (Star r)  = True
